734d86dffa7a631510b394a2bfe622d9ec4b624e
johnbpetersen <john.b.petersen@gmail.com>
2012-06-16 18:08:50 -0600

    rails new wit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wit::Application.config.secret_token = '460c2c73d19b7a1f81f412abb3a237304eefdfd20e0bf595a3576a41e03e7cf576ed503f423c44f87e2122ef6fc7bc70e1f40d5cb1524723abc652b6649a1e3e'
