7d9a88c9029d326fe7dee20a700a611ea9e5948a
johnbpetersen <john.b.petersen@gmail.com>
2012-06-16 21:47:14 -0600

    Redirect to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Wit::Application.routes.draw do
 
   devise_for :users
 
-  root :to => "pages#home"
+  root :to => redirect("/users/sign_up")
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
