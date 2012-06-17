2ae15a202219016cd66305070672c2b6644e9c6c
johnbpetersen <john.b.petersen@gmail.com>
2012-06-16 21:59:17 -0600

    rails generate devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Wit::Application.routes.draw do
 
   devise_for :users
 
-  root :to => "devise/sessons#new"
+  root :to => "pages#home"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
